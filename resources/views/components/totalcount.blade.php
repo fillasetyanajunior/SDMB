209547b8fbcc467f54e3de9e963f01aca5068599
fillajunior <pantailovina@gmail.com>
2020-11-27 13:36:02 +0800

    Update 27/11/20

--- resources/views/components/totalcount.blade.php
+++ resources/views/components/totalcount.blade.php
@@ -4,7 +4,25 @@
             <div class="latest_newsarea"> <span>Latest News</span>
             <ul id="ticker01" class="news_sticker">
                 @foreach ($post as $item)
-                <li><a href="#"><img src="{{asset($item->foto)}}" alt="">{{$item->judul}}</a></li>
+                    @if ($postss->post == "aisyiyah")
+                    <li><a href="" ></a><img src="{{asset('aisyiyah/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "tokoh")
+                    <li><a href=""><img src="{{asset('tokoh/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "milenial")
+                    <li><a href=""><img src="{{asset('milenial/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "kiprah")
+                    <li><a href=""><img src="{{asset('kiprah/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "seni")
+                    <li><a href=""><img src="{{asset('seni/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "saudagar")
+                    <li><a href=""><img src="{{asset('saudagar/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "infopersyarikatan")
+                    <li><a href=""><img src="{{asset('infopersyarikatan/' . $postss->foto)}}" ></a></li>
+                    @elseif ($postss->post == "artikel")
+                    <li><a href=""><img src="{{asset('artikel/' . $postss->foto)}}" ></a></li>
+                    @else
+
+                    @endif
                 @endforeach
             </ul>
             <div class="social_area">
