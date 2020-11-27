b919ee66652199104ab2ac9813e30ab78b2e8b92
fillajunior <pantailovina@gmail.com>
2020-11-27 13:42:36 +0800

    Update 27/11/20

--- resources/views/components/totalcount.blade.php
+++ resources/views/components/totalcount.blade.php
@@ -5,21 +5,21 @@
             <ul id="ticker01" class="news_sticker">
                 @foreach ($post as $item)
                     @if ($item->post == "aisyiyah")
-                    <li><a href="" ></a><img src="{{asset('aisyiyah/' . $item->foto)}}" ></a></li>
+                    <li><a href="" ></a><img src="{{asset('aisyiyah/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "tokoh")
-                    <li><a href=""><img src="{{asset('tokoh/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('tokoh/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "milenial")
-                    <li><a href=""><img src="{{asset('milenial/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('milenial/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "kiprah")
-                    <li><a href=""><img src="{{asset('kiprah/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('kiprah/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "seni")
-                    <li><a href=""><img src="{{asset('seni/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('seni/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "saudagar")
-                    <li><a href=""><img src="{{asset('saudagar/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('saudagar/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "infopersyarikatan")
-                    <li><a href=""><img src="{{asset('infopersyarikatan/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('infopersyarikatan/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @elseif ($item->post == "artikel")
-                    <li><a href=""><img src="{{asset('artikel/' . $item->foto)}}" ></a></li>
+                    <li><a href=""><img src="{{asset('artikel/' . $item->foto)}}" >{{$item->judul}}</a></li>
                     @else
 
                     @endif
